b206c5099d956be6334365f133c83ecabd310b08
tim-external <timhansen93@googlemail.com>
2024-10-17 16:58:38 +0200

    back to classical Docker ros without GPU and Pytorch. Later only cpu based pytorch

--- pythonScripts/matchingProfiling/testingSoftOnPredatorData.py
+++ pythonScripts/matchingProfiling/testingSoftOnPredatorData.py
@@ -2,6 +2,23 @@ import os, torch, json, argparse, shutil
 from predator.datasets.dataloader import get_dataloader, get_datasets
 from easydict import EasyDict as edict
 from predator.lib.utils import setup_seed, load_config
+# ros include stuff
+import rclpy
+from rclpy.node import Node
+import numpy
+from fsregistration.srv import request_list_potential_solution3_d
+
+class MinimalService(Node):
+
+    def __init__(self):
+        super().__init__('minimal_service')
+        self.srv = self.create_service(AddTwoInts, 'add_two_ints', self.add_two_ints_callback)
+
+    def add_two_ints_callback(self, request, response):
+        response.sum = request.a + request.b
+        self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
+
+        return response
 
 
 architectures = dict()
@@ -59,9 +76,7 @@ if __name__ == '__main__':
 
     inputs = next(iter(config.train_loader))
 
-    inputs.shape
 
-    print("test")
 
 
 
