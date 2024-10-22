c51c5ef67c7c7da1b2360cd05abdf4a51f224019
tim-external <timhansen93@googlemail.com>
2024-10-22 15:41:33 +0200

    working open3d map with plotting of Data.

--- pythonScripts/matchingProfiling/testingSoftOnPredatorData.py
+++ pythonScripts/matchingProfiling/testingSoftOnPredatorData.py
@@ -5,9 +5,10 @@ from predator.lib.utils import setup_seed, load_config
 # ros include stuff
 import rclpy
 from rclpy.node import Node
-import numpy
-from fsregistration.srv import request_list_potential_solution3_d
-
+import numpy as np
+from fsregistration.srv import _request_list_potential_solution3_d
+import open3d as o3d
+import copy
 class MinimalService(Node):
 
     def __init__(self):
@@ -19,6 +20,15 @@ class MinimalService(Node):
         self.get_logger().info('Incoming request\na: %d b: %d' % (request.a, request.b))
 
         return response
+
+def draw_registration_result(source, target, transformation):
+    source_temp = copy.deepcopy(source)
+    target_temp = copy.deepcopy(target)
+    source_temp.paint_uniform_color([1, 0.706, 0])
+    target_temp.paint_uniform_color([0, 0.651, 0.929])
+    source_temp.transform(transformation)
+    o3d.visualization.draw_geometries([source_temp, target_temp])
+
 
 
 architectures = dict()
@@ -63,18 +73,37 @@ if __name__ == '__main__':
                                                               num_workers=config.num_workers,
                                                               )
     config.val_loader, _ = get_dataloader(dataset=val_set,
-                                          batch_size=config.batch_size,
+                                           batch_size=config.batch_size,
                                           shuffle=False,
-                                          num_workers=1,
+                                          num_workers=0,
                                           neighborhood_limits=neighborhood_limits
                                           )
     config.test_loader, _ = get_dataloader(dataset=benchmark_set,
                                            batch_size=config.batch_size,
                                            shuffle=False,
-                                           num_workers=1,
+                                           num_workers=0,
                                            neighborhood_limits=neighborhood_limits)
 
-    inputs = next(iter(config.train_loader))
+
+
+
+    for _ in range(10):
+        inputs = next(iter(config.train_loader))
+
+        # Pass xyz to Open3D.o3d.geometry.PointCloud and visualize
+        pcd1 = o3d.geometry.PointCloud()
+        pcd2 = o3d.geometry.PointCloud()
+        pcd1.points = o3d.utility.Vector3dVector(inputs["src_pcd_raw"])
+        pcd2.points = o3d.utility.Vector3dVector(inputs["tgt_pcd_raw"])
+        # draw_registration_result(pcd1,pcd2,np.identity(4))
+
+        T = np.eye(4)
+        T[:3, :3] = inputs["rot"]
+        T[:3, 3] = np.squeeze(np.asarray(inputs["trans"]))
+        print(T)
+        draw_registration_result(pcd1,pcd2,T)
+
+        print("test")
 
 
 
