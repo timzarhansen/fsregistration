ebfd80b356903d00bc39649d785e79fd68c39636
tim-external <timhansen93@googlemail.com>
2024-11-06 12:17:38 +0100

    short update

---
+++ pythonScripts/matchingProfiling/runScripts/allRunScripts/input3.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 8 24 0.01 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 8 24 0.01 0.01 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 8 24 0.001 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 8 24 0.001 0.01 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 16 56 0.01 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 16 56 0.01 0.01 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 16 56 0.001 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 16 56 0.001 0.01 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 32 96 0.01 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 32 96 0.01 0.01 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 32 96 0.001 0.1 &
+nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 32 96 0.001 0.01 &
+
+
+
