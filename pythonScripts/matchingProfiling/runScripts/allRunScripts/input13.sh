819472e6d09c97958a533ee381dcf5e9c0502134
tim-external <timhansen93@googlemail.com>
2024-11-08 14:16:23 +0100

    changed path for 3d match data string

--- pythonScripts/matchingProfiling/runScripts/allRunScripts/input13.sh
+++ pythonScripts/matchingProfiling/runScripts/allRunScripts/input13.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 source /opt/ros/humble/setup.bash
 source /home/tim-external/ros_ws/install/setup.bash
-ros2 run fsregistration ros2ServiceRegistrationFS3D &>/dev/null
+ros2 run fsregistration ros2ServiceRegistrationFS3D & >/dev/null 2>&1
 cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling/
 
-./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.01 0.1 &>/dev/null
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.01 0.1 & >/dev/null 2>&1
 pid1=$!
-./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.01 0.01 &>/dev/null
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.01 0.01 & >/dev/null 2>&1
 pid2=$!
-./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.001 0.1 &>/dev/null
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.001 0.1 & >/dev/null 2>&1
 pid3=$!
-./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.001 0.01 &>/dev/null
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 1 16 96 0.001 0.01 & >/dev/null 2>&1
 pid4=$!
 
 wait $pid1 $pid2 $pid3 $pid4
