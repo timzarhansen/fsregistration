48ce3629148b79d9e465ca1b2eb9d551d527fd53
tim-external <timhansen93@googlemail.com>
2024-11-08 11:27:00 +0100

    test for other stuff

--- pythonScripts/matchingProfiling/runScripts/allRunScripts/input1.sh
+++ pythonScripts/matchingProfiling/runScripts/allRunScripts/input1.sh
@@ -1,34 +1,19 @@
 #! /bin/bash
 source /opt/ros/humble/setup.bash
 source /home/tim-external/ros_ws/install/setup.bash
-ros2 run fsregistration ros2ServiceRegistrationFS3D &
+ros2 run fsregistration ros2ServiceRegistrationFS3D &>/dev/null
 cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling/
 
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 28 0.01 0.1 &
-pid1=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 28 0.01 0.01 &
-pid2=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 28 0.001 0.1 &
-pid3=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 28 0.001 0.01 &
-pid4=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 48 0.01 0.1 &
-pid5=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 48 0.01 0.01 &
-pid6=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 48 0.001 0.1 &
-pid7=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 48 0.001 0.01 &
-pid8=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.01 0.1 &
+
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.01 0.1 &>/dev/null
 pid9=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.01 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.01 0.01 &>/dev/null
 pid10=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.001 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.001 0.1 &>/dev/null
 pid11=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.001 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 96 0.001 0.01 &>/dev/null
 pid12=$!
 #ros2 service list
 
 
-wait $pid1 $pid2 $pid3 $pid4 $pid5 $pid6 $pid7 $pid8
+wait $pid9 $pid10 $pid11 $pid12
--- pythonScripts/matchingProfiling/runScripts/allRunScripts/input2.sh
+++ pythonScripts/matchingProfiling/runScripts/allRunScripts/input2.sh
@@ -1,32 +1,32 @@
 #! /bin/bash
 source /opt/ros/humble/setup.bash
 source /home/tim-external/ros_ws/install/setup.bash
-ros2 run fsregistration ros2ServiceRegistrationFS3D &
+ros2 run fsregistration ros2ServiceRegistrationFS3D &>/dev/null
 cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling/
 
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.01 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.01 0.1 &>/dev/null
 pid1=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.01 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.01 0.01 &>/dev/null
 pid2=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.001 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.001 0.1 &>/dev/null
 pid3=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.001 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 32 0 4 24 0.001 0.01 &>/dev/null
 pid4=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.01 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.01 0.1 &>/dev/null
 pid5=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.01 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.01 0.01 &>/dev/null
 pid6=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.001 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.001 0.1 &>/dev/null
 pid7=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.001 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 64 0 8 56 0.001 0.01 &>/dev/null
 pid8=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.01 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.01 0.1 &>/dev/null
 pid9=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.01 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.01 0.01 &>/dev/null
 pid10=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.001 0.1 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.001 0.1 &>/dev/null
 pid11=$!
-nohup python3 testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.001 0.01 &
+./testingSoftOnPredatorData.py configFiles/predatorNothing.yaml 128 0 16 112 0.001 0.01 &>/dev/null
 pid12=$!
 
 wait $pid1 $pid2 $pid3 $pid4 $pid5 $pid6 $pid7 $pid8 pid9=$! pid10=$! pid11=$! pid12=$!
