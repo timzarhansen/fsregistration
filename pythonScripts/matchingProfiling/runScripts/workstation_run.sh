55701ec6ccb6b8b713b9e9639cf6ded08ba5a2d2
timhansen <timhansen93@googlemail.com>
2024-11-15 12:00:34 +0100

    update regarding mac stuff

--- pythonScripts/matchingProfiling/runScripts/workstation_run.sh
+++ pythonScripts/matchingProfiling/runScripts/workstation_run.sh
@@ -1,9 +1,21 @@
 #! /bin/bash
 
-cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/installcolcon.sh
-
 cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input1.sh
-
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input2.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input3.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input4.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input5.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input6.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input7.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input8.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input9.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input10.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input11.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input12.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input13.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input14.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input15.sh
+cd /home/tim-external/ros_ws/src/fsregistration/pythonScripts/matchingProfiling && bash runScripts/allRunScripts/input16.sh
 
 
 
